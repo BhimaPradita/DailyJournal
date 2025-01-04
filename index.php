44c9aea5324c3e75c5e460fef515c2c760250423
redhie <redhieredcat@gmail.com>
2025-01-04 19:39:07 +0700

    ubah judul form login

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "koneksi.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daily Journal</title>
+    <title>Daily Journal cuy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
